9c998030a4ed6a602bb698ab2d1e3687ff776bba
Himanshu Pandey <himanshu31099@gmail.com>
2020-07-11 02:42:34 +0530

    Added Ml model framework used in the project for reference

---
+++ mars_colonization/tictactoe/ml_models/model.py
@@ -0,0 +1,16 @@
+# from keras.models import Sequential
+# from keras.layers import Dense, Flatten
+# from keras.models import load_model
+# from keras import optimizers
+
+
+# Thi is the structure of ML model used in our project for Neural Network based algorith
+
+
+# model = Sequential()
+# model.add(Dense(units=130, activation='relu', input_dim=27, kernel_initializer='random_uniform', bias_initializer='zeros'))
+# model.add(Dense(units=250, activation='relu', kernel_initializer='random_uniform', bias_initializer='zeros'))
+# model.add(Dense(units=140, activation='relu', kernel_initializer='random_uniform', bias_initializer='zeros'))
+# model.add(Dense(units=60, activation='relu', kernel_initializer='random_uniform', bias_initializer='zeros'))
+# model.add(Dense(9, kernel_initializer='random_uniform', bias_initializer='zeros'))
+# model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
